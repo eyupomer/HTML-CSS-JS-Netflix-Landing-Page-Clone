fbb26ea138caf1a4180d3882eafddd370739a218
eyupomer <odabasieyupomer@gmail.com>
2022-05-06 16:40:49 +0300

    finished

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add('show')
+};
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+};
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+};
+
+// Lister for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
